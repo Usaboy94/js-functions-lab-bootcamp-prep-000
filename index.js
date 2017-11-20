556c8ddaa8d31e2fcbe3276a36b061fa26f38bed
Olivier T <oliviertemmerman@hotmail.com>
2017-11-20 16:23:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ function happyHolidaysTo(name) {
 function happyHolidayTo(holiday, name) {
   return 'Happy ${holiday}, ${name}!'
 }
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
